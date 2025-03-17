46fe40b9943d0f161150abae751316ba38c6ee0f
Stitch <dustin.poetzsch@tn.techstarter.de>
2025-03-17 18:12:55 +0100

    update

---
+++ Untterricht_Lukas/createHTMLList.js
@@ -0,0 +1,10 @@
+funktion createHTMLList(list){
+    let ergebnis = ""
+
+    let htmlElemnts = liste.map((listtItem) => `<li>${listItem}</li>`)
+    console.log(htmlElemnts)
+    return ergebnis
+}
+
+my_list = ["Nova", "Toni"]
+createHTMLList(my_list)
